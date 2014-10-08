2a6fbe4a2de11f7a4e328e195bca6aaec6e761e4
nemolovich <nemolovich@hotmail.fr>
2014-10-09 00:17:02 +0200

    Fixed .gitignore

---
+++ src/main/java/fr/nemolovich/apps/homeapp/reflection/SearchFilter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fr.nemolovich.apps.homeapp.reflection;
+
+/**
+ *
+ * @author Nemolovich
+ */
+public abstract class SearchFilter {
+    public abstract boolean filterMatches(Class<?> clazz);
+}
