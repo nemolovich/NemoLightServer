3cc85bcf3b17bb9132489d59a215a3bc50b7a847
nemolovich <nemolovich@hotmail.fr>
2014-10-29 19:31:44 +0100

    Need fix the response reader to display error from gui

--- HomeAppAdminGUI/src/main/java/fr/nemolovich/apps/homeapp/admin/gui/commands/CommandsUtil.java
+++ HomeAppAdminGUI/src/main/java/fr/nemolovich/apps/homeapp/admin/gui/commands/CommandsUtil.java
@@ -5,12 +5,11 @@
  */
 package fr.nemolovich.apps.homeapp.admin.gui.commands;
 
+import fr.nemolovich.apps.homeapp.admin.Command;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-import fr.nemolovich.apps.homeapp.admin.Command;
 
 /**
  *
@@ -18,42 +17,46 @@ import fr.nemolovich.apps.homeapp.admin.Command;
  */
 public class CommandsUtil {
 
-	private static final List<Command> INTERNAL_COMMANDS;
-	private static final List<String> SERVER_COMMANDS;
+    private static final List<Command> INTERNAL_COMMANDS;
+    private static final List<String> SERVER_COMMANDS;
 
-	static {
-		INTERNAL_COMMANDS = Collections
-				.synchronizedList(new ArrayList<Command>());
-		SERVER_COMMANDS = Collections.synchronizedList(new ArrayList<String>());
-	}
+    static {
+        INTERNAL_COMMANDS = Collections
+            .synchronizedList(new ArrayList<Command>());
+        SERVER_COMMANDS = Collections.synchronizedList(new ArrayList<String>());
+    }
 
-	public static void addCommand(Command command) {
-		INTERNAL_COMMANDS.add(command);
-	}
+    public static void addCommand(Command command) {
+        INTERNAL_COMMANDS.add(command);
+    }
 
-	public static void addServerCommand(String... commands) {
-		SERVER_COMMANDS.addAll(Arrays.asList(commands));
-	}
+    public static void addServerCommand(String... commands) {
+        SERVER_COMMANDS.addAll(Arrays.asList(commands));
+    }
 
-	public static final List<String> getAvailableCommands() {
-		List<String> result = new ArrayList();
-		for (Command command : INTERNAL_COMMANDS) {
-			result.add(command.getCommandName());
-		}
-		result.addAll(SERVER_COMMANDS);
+    public static void addServerCommand(List<String> commands) {
+        SERVER_COMMANDS.addAll(commands);
+    }
 
-		return result;
-	}
+    public static final List<String> getAvailableCommands() {
+        List<String> result = new ArrayList();
+        for (Command command : INTERNAL_COMMANDS) {
+            result.add(command.getCommandName());
+        }
+        result.addAll(SERVER_COMMANDS);
 
-	public static String getInternalCommandHelp(String commandName) {
-		String result=null;
-		for(Command command:INTERNAL_COMMANDS) {
-			if(command.getCommandName().equalsIgnoreCase(commandName)) {
-				result=command.getHelp();
-				break;
-			}
-		}
-		return result;
-	}
-	
+        return result;
+    }
+
+    public static String getInternalCommandHelp(String commandName) {
+        String result = null;
+        for (Command command : INTERNAL_COMMANDS) {
+            if (command.getCommandName().equalsIgnoreCase(commandName)) {
+                result = command.getHelp();
+                break;
+            }
+        }
+        return result;
+    }
+
 }
