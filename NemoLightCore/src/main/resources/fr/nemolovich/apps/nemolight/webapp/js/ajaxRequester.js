789ddb34a4d49f5b78c1997e264a1361853f9360
Nemolovich <nemolovich@hotmail.fr>
2015-02-22 01:33:32 +0100

    Refractored Ajax behavior.

---
+++ NemoLightCore/src/main/resources/fr/nemolovich/apps/nemolight/webapp/js/ajaxRequester.js
@@ -0,0 +1,85 @@
+AJAX_FIELD_SUFFIX="_ajax_input";
+
+function request(bean, fields, callback) {
+	if (!beanExists(bean)) {
+		console.error("The bean '" + bean + "' does not exist!");
+		return;
+	}
+	var value = getAjaxValues(fields);
+	var uuid = generateUUID();
+	var url = "/ajax/" + uuid;
+	$.ajax({
+		type: 'POST',
+		url: url,
+		data: {
+			bean: bean,
+			uid: uuid,
+			value: JSON.stringify(value)
+		},
+		success: function (response) {
+			try {
+				var r = response.replace(/\n/g, '').replace(/\r/g, '')
+						.replace(/\/\*{2}.*\*\//, '');
+				var resp = JSON.parse(r);
+				if (resp.action === 'update') {
+					updateFields(resp.value);
+				}
+				if (callback !== undefined) {
+					callback();
+				}
+			} catch (e) {
+				console.error("Can not parse result: " + e);
+			}
+		},
+		error: function (xhr, ajaxOptions, thrownError) {
+			console.error(xhr.status); // 0
+			console.error(ajaxOptions);
+			console.error(thrownError);
+		}
+	});
+	return false;
+}
+
+function getAjaxValues(fields) {
+	var result = new Object();
+	for (i = 0; i < fields.length; i++) {
+		var value = document.getElementById(fields[i] +
+				AJAX_FIELD_SUFFIX);
+		if (value !== null) {
+			result[fields[i]] = value.value;
+		}
+	}
+	return result;
+}
+
+function generateUUID() {
+	var d = new Date().getTime();
+	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+		var r = (d + Math.random() * 16) % 16 | 0;
+		d = Math.floor(d / 16);
+		return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+	});
+	return uuid;
+}
+
+function updateFields(value) {
+	for (var key in value) {
+		if (value.hasOwnProperty(key)) {
+			var elm = document.getElementById(key);
+			if (elm !== null && elm !== undefined) {
+				elm.innerHTML = value[key];
+			}
+		}
+	}
+}
+
+function log(txt) {
+	console.log(txt);
+}
+
+function beanExists(bean) {
+	/*
+	 * TODO: Search in 2 maps
+	 */
+	return true;
+}
