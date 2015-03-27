fdc444cbcdb67b6cc7545893fea5a31a08788368
nemolovich <nemolovich@hotmail.fr>
2015-03-27 14:06:55 +0100

    Remove file 'test.xml" after tests.

--- NemoLightCore/src/test/java/fr/nemolovich/apps/nemolight/tests/TestConfig.java
+++ NemoLightCore/src/test/java/fr/nemolovich/apps/nemolight/tests/TestConfig.java
@@ -1,18 +1,25 @@
 package fr.nemolovich.apps.nemolight.tests;
 
 import fr.nemolovich.apps.nemolight.route.file.utils.Config;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.xml.bind.JAXBException;
+
+import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  *
@@ -20,28 +27,38 @@ import org.junit.runners.MethodSorters;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestConfig {
-	
-	private static final Config filesPath = new Config();
-	private static final File file = new File("test.xml");
-	
+
+	private static final Config FILES_PATH = new Config();
+	private static final File FILE = new File("test.xml").getAbsoluteFile();
+
 	@BeforeClass
 	public static void init() {
 		List<String> paths = new ArrayList<>();
 		paths.add("C:/");
 		paths.add("F:/");
-		filesPath.put(Config.DEPLOY_FILES_PATH, paths);
-		filesPath.put("testString", "A string");
+		FILES_PATH.put(Config.DEPLOY_FILES_PATH, paths);
+		FILES_PATH.put("testString", "A string");
 	}
-	
+
 	@Test
-	public void test1() throws JAXBException, FileNotFoundException {
-		filesPath.saveConfig(new FileOutputStream(file));
+	public void test1() throws JAXBException, IOException {
+		FileOutputStream os = new FileOutputStream(FILE);
+		FILES_PATH.saveConfig(os);
+		os.close();
 	}
-	
+
 	@Test
-	public void test2() throws JAXBException, FileNotFoundException {
-		Config c = Config.loadConfig(new FileInputStream(file));
-		Assert.assertEquals(c.get("testString"),
-			filesPath.get("testString"));
+	public void test2() throws JAXBException, IOException {
+		FileInputStream is = new FileInputStream(FILE);
+		Config c = Config.loadConfig(is);
+		Assert.assertEquals(c.get("testString"), FILES_PATH.get("testString"));
+		is.close();
+	}
+
+	@AfterClass
+	public static void close() {
+		assertTrue(FILE.exists());
+		boolean deleted = FILE.delete();
+		 assertTrue(deleted);
 	}
 }
